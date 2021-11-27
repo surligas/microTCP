94f8b55beacb87a5db76699f70b355b64e75b594
GEORGIOS PAPOUTSAKIS <csd4354@ikaria.csd.uoc.gr>
2021-11-27 19:34:13 +0200

    okk

--- lib/microtcp.c
+++ lib/microtcp.c
@@ -29,17 +29,15 @@ microtcp_sock_t
 microtcp_socket (int domain, int type, int protocol)
 {
 
- microtcp_sock_t s1;
+ 	microtcp_sock_t s1;
+	//s1.sd = socket(domain, SOCK_DGRAM, IPPROTO_UDP);
 
-
-	if ( ( s1 = socket( AF_INET , SOCK_STREAM, IPPROTO_TCP ) ) == -1){
+	if ( ( s1.sd = socket( AF_INET , SOCK_STREAM, IPPROTO_TCP ) ) == -1){
 		perror("opening TCP listening socket");
 		exit(EXIT_FAILURE);
 	}
 
-	microtcp_state_t state; //**< The state of the microTCP socket */
-
-
+	s1.state = UKNOWN;
 
   /* Your code here */
 }
