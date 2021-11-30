5c2f9cbac62eb6509f3cbc5325ff6164c9ff0e5c
EMMANOUIL KARYDIS <csd4417@cristina.csd.uoc.gr>
2021-11-30 19:35:51 +0200

    geia

--- lib/microtcp.c
+++ lib/microtcp.c
@@ -102,18 +102,21 @@ microtcp_accept (microtcp_sock_t *socket, struct sockaddr *address,
                  socklen_t address_len)
 {
 	microtcp_header_t header;
+	struct sockaddr* restrict addr=address;
+	socklen_t* restrict addr_len=&address_len;
 	int flags=0;
 	int rec;
 	socket->recvbuf=(uint8_t*)malloc(MICROTCP_RECVBUF_LEN*sizeof(uint8_t));
-	if(rec=recvfrom(socket->sd,&header,MICROTCP_RECVBUF_LEN,flags,address,&address_len)==-1){
+	if(rec=recvfrom(socket->sd,socket->recvbuf,MICROTCP_RECVBUF_LEN,flags,addr,addr_len)==-1){
 		perror("TCP Accept first receival\n");
 		socket->state=INVALID;
 		return -1;
 	}
-	if(header->control!=(uint8_t)(0*ACK+1*SYN+0*FIN)){
+	if(socket->recvbuf[8]=htons((uint8_t)(0*ACK+1*SYN+0*FIN))){
 		printf("Client did not send SYN for the handshake\n");
 		return -1;
 	}
+	socket->recvbuf[8]=htons(uint8_t)(0*ACK+0*SYN+0*FIN))
 
 	return 0;
 }
