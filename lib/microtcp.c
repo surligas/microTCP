e1309f9b744262f942969d551459c08c2255e58a
GEORGIOS PAPOUTSAKIS <csd4354@ikaria.csd.uoc.gr>
2021-11-30 19:01:43 +0200

    ok

--- lib/microtcp.c
+++ lib/microtcp.c
@@ -31,12 +31,7 @@ microtcp_socket (int domain, int type, int protocol)
 {
 
  	microtcp_sock_t s1;
-	if ( ( s1.sd = socket(domain ,type,protocol ) ) == -1){
-		perror("opening TCP listening socket\n");
-	}
-
-	s1.state = UKNOWN;
-	return s1;   
+ 
 	if ( ( s1.sd = socket(domain ,type,protocol ) ) == -1){
 		perror("opening TCP listening socket\n");
 		s1.state = INVALID;
