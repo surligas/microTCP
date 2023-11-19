b2b80654eee4f6563583112bb93ac9634a5e0df8
Thanasis <csd4702@csd.uoc.gr>
2023-11-19 19:14:58 +0200

    Updated socket, bind and test files

--- lib/microtcp.h
+++ lib/microtcp.h
@@ -21,6 +21,8 @@
 #ifndef LIB_MICROTCP_H_
 #define LIB_MICROTCP_H_
 
+#include <stdlib.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdint.h>
@@ -43,12 +45,14 @@
  */
 typedef enum
 {
-  LISTEN,
-  ESTABLISHED,
-  CLOSING_BY_PEER,
-  CLOSING_BY_HOST,
-  CLOSED,
-  INVALID
+    UNBDOUND,
+    BINDED,
+    LISTEN,
+    ESTABLISHED,
+    CLOSING_BY_PEER,
+    CLOSING_BY_HOST,
+    CLOSED,
+    INVALID
 } mircotcp_state_t;
 
 
@@ -60,28 +64,28 @@ typedef enum
  */
 typedef struct
 {
-  int sd;                       /**< The underline UDP socket descriptor */
-  mircotcp_state_t state;       /**< The state of the microTCP socket */
-  size_t init_win_size;         /**< The window size negotiated at the 3-way handshake */
-  size_t curr_win_size;         /**< The current window size */
+    int sd;                       /**< The underline UDP socket descriptor */
+    mircotcp_state_t state;       /**< The state of the microTCP socket */
+    size_t init_win_size;         /**< The window size negotiated at the 3-way handshake */
+    size_t curr_win_size;         /**< The current window size */
 
-  uint8_t *recvbuf;             /**< The *receive* buffer of the TCP
-                                     connection. It is allocated during the connection establishment and
-                                     is freed at the shutdown of the connection. This buffer is used
-                                     to retrieve the data from the network. */
-  size_t buf_fill_level;        /**< Amount of data in the buffer */
+    uint8_t *recvbuf;             /**< The *receive* buffer of the TCP
+                                        connection. It is allocated during the connection establishment and
+                                        is freed at the shutdown of the connection. This buffer is used
+                                        to retrieve the data from the network. */
+    size_t buf_fill_level;        /**< Amount of data in the buffer */
 
-  size_t cwnd;
-  size_t ssthresh;
+    size_t cwnd;
+    size_t ssthresh;
 
-  size_t seq_number;            /**< Keep the state of the sequence number */
-  size_t ack_number;            /**< Keep the state of the ack number */
-  uint64_t packets_send;
-  uint64_t packets_received;
-  uint64_t packets_lost;
-  uint64_t bytes_send;
-  uint64_t bytes_received;
-  uint64_t bytes_lost;
+    size_t seq_number;            /**< Keep the state of the sequence number */
+    size_t ack_number;            /**< Keep the state of the ack number */
+    uint64_t packets_send;
+    uint64_t packets_received;
+    uint64_t packets_lost;
+    uint64_t bytes_send;
+    uint64_t bytes_received;
+    uint64_t bytes_lost;
 } microtcp_sock_t;
 
 
@@ -91,15 +95,15 @@ typedef struct
  */
 typedef struct
 {
-  uint32_t seq_number;          /**< Sequence number */
-  uint32_t ack_number;          /**< ACK number */
-  uint16_t control;             /**< Control bits (e.g. SYN, ACK, FIN) */
-  uint16_t window;              /**< Window size in bytes */
-  uint32_t data_len;            /**< Data length in bytes (EXCLUDING header) */
-  uint32_t future_use0;         /**< 32-bits for future use */
-  uint32_t future_use1;         /**< 32-bits for future use */
-  uint32_t future_use2;         /**< 32-bits for future use */
-  uint32_t checksum;            /**< CRC-32 checksum, see crc32() in utils folder */
+    uint32_t seq_number;          /**< Sequence number */
+    uint32_t ack_number;          /**< ACK number */
+    uint16_t control;             /**< Control bits (e.g. SYN, ACK, FIN) */
+    uint16_t window;              /**< Window size in bytes */
+    uint32_t data_len;            /**< Data length in bytes (EXCLUDING header) */
+    uint32_t future_use0;         /**< 32-bits for future use */
+    uint32_t future_use1;         /**< 32-bits for future use */
+    uint32_t future_use2;         /**< 32-bits for future use */
+    uint32_t checksum;            /**< CRC-32 checksum, see crc32() in utils folder */
 } microtcp_header_t;
 
 
@@ -136,6 +140,5 @@ microtcp_send (microtcp_sock_t *socket, const void *buffer, size_t length,
 
 ssize_t
 microtcp_recv (microtcp_sock_t *socket, void *buffer, size_t length, int flags);
-
 
 #endif /* LIB_MICROTCP_H_ */
--- test/test_microtcp_client.c
+++ test/test_microtcp_client.c
@@ -23,8 +23,77 @@
  * This file is already inserted at the build system.
  */
 
-int
-main(int argc, char **argv)
-{
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<netdb.h>  
+#include<unistd.h>
+#include<string.h>
+#include<stdlib.h>
+#include "../lib/microtcp.h"
 
+int main(int argc,char **argv){
+    microtcp_sock_t clientSocket; /* Socket Decriptor for Client */
+    struct sockaddr_in server_addr;
+    struct hostent *ptrh;
+
+    char message[100];
+    char received[100];
+    int n = 0;
+
+    //Check if port number is given
+    if(argv[1] == NULL){
+        perror("No port added!Execute the command with \"test_microtcp_client [port_number]\" ");
+        exit( EXIT_FAILURE );
+    }
+    clientSocket = microtcp_socket(AF_INET, SOCK_DGRAM, 0);
+    if(clientSocket.sd == -1){
+        perror ( " SOCKET COULD NOT BE OPENED " );
+        exit ( EXIT_FAILURE );
+    }
+    
+    // clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    memset((char*)&server_addr, 0, sizeof(server_addr));
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(atoi(argv[1]));
+
+    /*  bind(clientSocket, (struct sockaddr*)&server_addr, sizeof(struct sockaddr)); */
+
+    ptrh = gethostbyname("127.0.0.1");
+    memcpy(&server_addr.sin_addr,ptrh->h_addr,ptrh->h_length);
+
+    if( -1 == connect(clientSocket.sd, (struct sockaddr*)&server_addr, sizeof(server_addr))){
+        printf("\nServer Not Ready !!\n"); exit(1);
+    }
+
+    while(1)
+    {
+        printf("\nUser:-");
+        // memset(message, '\0', 10);
+
+        gets(message);
+
+        n = write(clientSocket.sd, message, strlen(message)+1);
+        if( (strcmp(message,"q") == 0 ) || (strcmp(message,"Q") == 0 )){
+            printf("Wrong place...Socket Closed\n");
+            close(clientSocket.sd);
+            break;
+        }
+            //printf("Write:<%u>\n", n);
+
+        read(clientSocket.sd, received, sizeof(received));
+        if( (strcmp(received,"q") == 0 ) || (strcmp(received,"Q") == 0 )){
+            printf("Wrong place...Socket Closed\n");
+            close(clientSocket.sd);
+            break;
+        }
+        else{
+            printf("Server:- %s\n", received);
+        }
+    }
+
+    return 0;
 }
